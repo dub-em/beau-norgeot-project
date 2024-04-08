2f0bd01e12042166316a3c4e57f93ac05d953419
Dubem <michaeligbomezie@gmail.com>
2024-04-08 15:58:09 +0100

    Modified the automated testing

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -64,27 +64,20 @@ def test_biobert_similarity():
     text2 = "Please evaluate for potential arrhythmia and provide recommendations for further testing and management."
 
     biobert_sim = utilities.biobert_similarity(text1, text2, utilities.model, utilities.tokenizer)
-    assert biobert_sim == 0.9759095907211304
+    assert round(float(biobert_sim), 5) == round(float(0.9759095907211304), 5)
 
 
 def test_ensemble_similarity():
     text1 = "Please evaluate for potential cardiac causes of syncope and provide recommendations for further testing and management."
     text2 = "Please evaluate for potential arrhythmia and provide recommendations for further testing and management."
-    expected_list_1 = [0.6666666666666666, 0.9759095907211304, 0.774901690085729, 0.9090909090909091, 0.7142857142857143, 0.8]
+    expected_list_1 = [round(float(0.6666666666666666), 5), round(float(0.9759095907211304), 5), round(float(0.774901690085729), 5), round(float(0.9090909090909091), 5), round(float(0.7142857142857143), 5), 0.8]
     expected_list_2 = [['cardiac', 'of', 'syncope', 'cause'], ['arrhythmia']]
 
     jacc_sim, biobert_sim, ensemble_sim, precision, recall, f1_score, onlyin_original, onlyin_generated = utilities.ensemble_similarity(text1, text2, utilities.nlp, utilities.model, utilities.tokenizer)
-    result_list_1 = [jacc_sim, biobert_sim, ensemble_sim, precision, recall, f1_score]
+    result_list_1 = [round(float(jacc_sim), 5), round(float(biobert_sim), 5), round(float(ensemble_sim), 5), round(float(precision), 5), round(float(recall), 5), f1_score]
     result_list_2 = [onlyin_original, onlyin_generated]
     check_1 = [result_list_2[0][i] in expected_list_2[0] for i in range(len(result_list_2[0]))] + [expected_list_2[0][i] in result_list_2[0] for i in range(len(expected_list_2[0]))]
     check_2 = [result_list_2[1][i] in expected_list_2[1] for i in range(len(result_list_2[1]))] + [expected_list_2[1][i] in result_list_2[1] for i in range(len(expected_list_2[1]))]
     assert result_list_1 == expected_list_1
     assert False not in check_1
     assert False not in check_2
-
-
-    
-
-
-
-
